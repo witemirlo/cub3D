75f0dfcfe474cc75bbc5eec53a14e9a9d1ac05c0
witemirlo <jacoboberdugo@gmail.com>
2024-06-26 17:08:27 +0200

    [ADD] function to check correct file type

---
+++ src/parser/check_correct_file_type.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_correct_file_type.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jberdugo <jberdugo@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 15:09:03 by jberdugo          #+#    #+#             */
+/*   Updated: 2024/06/26 17:08:07 by jberdugo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * Returns 1 if name has corrext type
+ */
+int check_correct_file_type(char const *name)
+{
+	char	*tmp;
+	size_t	size;
+
+	if (!name)
+		return (0);
+	size = ft_strlen(name);
+	if (size < 5)
+		return (0);
+	tmp = ft_strnstr(name, ".cub", size);
+	if (!tmp)
+		return (0);
+	if (ft_strlen(tmp) != 4)
+		return (0);
+	return (1);
+}
