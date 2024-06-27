e994788f3cf00b4f72f9c9290c80eb3ae208b9fb
witemirlo <jacoboberdugo@gmail.com>
2024-06-27 20:17:34 +0200

    [WIP] function to make a first check of all file content

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: jberdugo <jberdugo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 14:10:55 by jberdugo          #+#    #+#             */
-/*   Updated: 2024/06/27 15:40:41 by jberdugo         ###   ########.fr       */
+/*   Updated: 2024/06/27 19:23:42 by jberdugo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "libft.h"
 
+/******************************************************************************/
 typedef struct s_texture_path
 {
 	char	*north;
@@ -25,8 +26,37 @@ typedef struct s_texture_path
 	char	*ceiling;
 }	t_texture_path;
 
+/******************************************************************************/
+/**
+ * mask cheatsheet:
+ *   00000000 00000001 -> 0x1        -> NORTH
+ *   00000000 00000010 -> 0x2        -> SOUTH
+ *   00000000 00000100 -> 0x4        -> WEST
+ *   00000000 00001000 -> 0x8        -> EAST
+ *   00000000 00010000 -> 0x10       -> FLOOR
+ *   00000000 00100000 -> 0x20       -> CEILING
+ *  -------------------
+ *   00000000 00111111 -> 0x3F       -> ALL_TEXTURES
+
+ *   100...........000 -> 0x80000000 -> FAILURE
+ */
+typedef enum e_check_flags
+{
+	F_NORTH = 0x1,
+	F_SOUTH = 0x2,
+	F_WEST = 0x4,
+	F_EAST = 0x8,
+	F_FLOOR = 0x10,
+	F_CEILING = 0x20,
+	ALL_TEXTURES = 0x3F,
+	FAILURE = 0x80000000,
+	//SUCCESS = 
+}	t_check_flags;
+
+/******************************************************************************/
 int				check_correct_file_type(char const *name);
 t_list			*read_map(char const *name);
+int				check_file_content(t_list *file);
 t_texture_path	*get_texture_path(t_list *raw_file);
 
 #endif
---
+++ src/parser/check_file_content.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_file_content.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jberdugo <jberdugo@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/27 15:42:25 by jberdugo          #+#    #+#             */
+/*   Updated: 2024/06/27 20:14:45 by jberdugo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "parser.h"
+
+static t_check_flags	check_correct_order(t_list *file);
+static t_check_flags	check_all_textures(t_list *file);
+
+/* check if the file has correct FORMAT (map correctness is not checked) */
+int	check_file_content(t_list *file)
+{
+	t_check_flags	mask;
+
+	mask = 0;
+	mask |= check_correct_order(file);
+	mask |= check_all_textures(file);
+	// TODO: que en floor y ceiling tenga un color
+	// TODO: que las texturas tengan una ruta valida
+	mask |= check_map(file);
+	// return (mask == SUCCESS);
+	return (mask); // TODO: temporal
+}
+
+/* check that exits one instruction per required texture */
+static t_check_flags	check_all_textures(t_list *file)
+{
+	char			*tmp;
+	t_check_flags	mask;
+
+	mask = 0;
+	while (file)
+	{
+		tmp = (char *)(file->content);
+		if (ft_strncmp(tmp, "NO ", 3))
+			mask ^= F_NORTH;
+		else if (ft_strncmp(tmp, "SO ", 3))
+			mask ^= F_SOUTH;
+		else if (ft_strncmp(tmp, "WE ", 3))
+			mask ^= F_WEST;
+		else if (ft_strncmp(tmp, "EA ", 3))
+			mask ^= F_EAST;
+		else if (ft_strncmp(tmp, "F ", 2))
+			mask ^= F_FLOOR;
+		else if (ft_strncmp(tmp, "C ", 2))
+			mask ^= F_CEILING;
+		file = file->next;
+	}
+	if ((mask & ALL_TEXTURES) != ALL_TEXTURES)
+		mask |= FAILURE;
+	return (mask);
+}
+
+static t_check_flags	check_correct_order(t_list *file)
+{
+	t_check_flags	mask;
+	char			*tmp;
+
+	mask = FAILURE;
+	while (file)
+	{
+		tmp = (char *)(file->content);
+		while (*tmp++ != '\0' && *tmp != ' ' && *tmp != '1')
+			tmp++;
+		if (*tmp == '\0')
+		{
+			mask = 0;
+			break ;
+		}
+		file = file->next;
+	}
+	while (file)
+	{
+		tmp = (char *)(file->content);
+		if (!ft_strrchr(" 01NSEW", *tmp))
+			mask = FAILURE;
+		file = file->next;
+	}
+	return (mask);
+}
+
+// TODO: comprobar caracteres
+int main(void) {
+	t_list tmp13 = (t_list){"11", NULL};
+	t_list tmp12 = (t_list){"101", &tmp13};
+	t_list tmp11 = (t_list){"101", &tmp12};
+	t_list tmp10 = (t_list){"101", &tmp11};
+	t_list tmp9  = (t_list){"111", &tmp10};
+
+	t_list tmp8  = (t_list){"", &tmp9};
+	t_list tmp7  = (t_list){"F ./path_to_texture", &tmp8};
+	t_list tmp6  = (t_list){"C ./path_to_texture", &tmp7};
+	t_list tmp5  = (t_list){"", &tmp6};
+	t_list tmp4  = (t_list){"EA ./path_to_texture", &tmp5};
+	t_list tmp3  = (t_list){"WE ./path_to_texture", &tmp4};
+	t_list tmp2  = (t_list){"SO ./path_to_texture", &tmp3};
+	t_list tmp1  = (t_list){"NO ./path_to_texture", &tmp2};
+}
