ea6a02b20edd3d1aa77723ff2c131d918795fadd
witemirlo <jacoboberdugo@gmail.com>
2024-06-28 13:48:50 +0200

    [ADD] checking that only exists one instruccion for each texture instrucction

--- src/parser/check_file_content.c
+++ src/parser/check_file_content.c
@@ -6,7 +6,7 @@
 /*   By: jberdugo <jberdugo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 15:42:25 by jberdugo          #+#    #+#             */
-/*   Updated: 2024/06/28 11:53:05 by jberdugo         ###   ########.fr       */
+/*   Updated: 2024/06/28 13:47:50 by jberdugo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 static t_check_flags	check_correct_order(t_list *file);
 static t_check_flags	check_all_textures(t_list *file);
+static t_check_flags	check_unique_textures(t_list *file);
 
 /* check if the file has correct FORMAT (map correctness is not checked) */
 int	check_file_content(t_list *file)
@@ -24,11 +25,8 @@ int	check_file_content(t_list *file)
 	mask = 0;
 	mask |= check_correct_order(file);
 	mask |= check_all_textures(file);
-	// TODO: que en floor y ceiling tenga un color
-	// TODO: que las texturas tengan una ruta valida
-	// mask |= check_map(file);
-	// return (mask == SUCCESS);
-	return (mask); // TODO: temporal
+	mask |= check_unique_textures(file);
+	return (mask == ALL_TEXTURES);
 }
 
 /* check that exits one instruction per required texture */
@@ -41,27 +39,48 @@ static t_check_flags	check_all_textures(t_list *file)
 	while (file)
 	{
 		tmp = (char *)(file->content);
-		if (ft_strncmp(tmp, "NO ", 3) && (mask & F_NORTH) != F_NORTH)
+		if (ft_strncmp(tmp, "NO ", 3))
 			mask |= F_NORTH;
-		else if (ft_strncmp(tmp, "SO ", 3) && (mask & F_SOUTH) != F_SOUTH)
+		else if (ft_strncmp(tmp, "SO ", 3))
 			mask |= F_SOUTH;
-		else if (ft_strncmp(tmp, "WE ", 3) && (mask & F_WEST) != F_WEST)
+		else if (ft_strncmp(tmp, "WE ", 3))
 			mask |= F_WEST;
-		else if (ft_strncmp(tmp, "EA ", 3) && (mask & F_EAST) != F_EAST)
+		else if (ft_strncmp(tmp, "EA ", 3))
 			mask |= F_EAST;
-		else if (ft_strncmp(tmp, "F ", 2) && (mask & F_FLOOR) != F_FLOOR)
+		else if (ft_strncmp(tmp, "F ", 2))
 			mask |= F_FLOOR;
-		else if (ft_strncmp(tmp, "C ", 2) && (mask & F_CEILING) != F_CEILING)
+		else if (ft_strncmp(tmp, "C ", 2))
 			mask |= F_CEILING;
-		else if (ft_strncmp(tmp, "NO ", 3) || ft_strncmp(tmp, "SO ", 3)
-			|| ft_strncmp(tmp, "WE ", 3) || ft_strncmp(tmp, "EA ", 3)
-			|| ft_strncmp(tmp, "F ", 3) || ft_strncmp(tmp, "C ", 3))
-			mask |= FAILURE | REPEATED;
 		file = file->next;
 	}
 	if ((mask & ALL_TEXTURES) != ALL_TEXTURES)
 		mask |= FAILURE;
 	return (mask);
+}
+
+/* check that only exists one instance of each texture instrucction */
+static t_check_flags	check_unique_textures(t_list *file)
+{
+	int		i;
+	int		count;
+	char	*str[6];
+
+	i = 0;
+	*str = (char *[6]){"NO ", "WE ", "EA ", "F ", "C ", NULL};
+	while (i < 6)
+	{
+		count = 0;
+		while (file)
+		{
+			if (ft_strnstr(file->content, str[i], ft_strlen(&str[i])))
+				count++;
+			file = file->next;
+		}
+		if (count > 1)
+			return (REPEATED);
+		i++;
+	}
+	return (0);
 }
 
 static t_check_flags	check_correct_order(t_list *file)
