b2b63caa87f80261a459d18038e19ac5841af818
witemirlo <jacoboberdugo@gmail.com>
2024-06-28 15:38:37 +0200

    [ADD] color header

---
+++ include/colors.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colors.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jberdugo <jberdugo@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/21 17:00:28 by jberdugo          #+#    #+#             */
+/*   Updated: 2024/04/27 12:02:10 by jberdugo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_H
+# define COLORS_H
+
+# define BLACK "\033[1;30m"
+# define RED "\033[1;31m"
+# define GREEN "\033[1;32m"
+# define YELLOW "\033[1;33m"
+# define BLUE "\033[1;34m"
+# define MAGENTA "\033[1;35m"
+# define CYAN "\033[1;36m"
+# define WHITE "\033[1;37m"
+# define NC "\033[0m"
+
+#endif
