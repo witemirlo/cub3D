d379e8e6a4f3d0cb514a5779ad9f17443e1c094e
witemirlo <jacoboberdugo@gmail.com>
2024-06-28 12:00:05 +0200

    [FIX] bug fix
    When a parameter was repeated 3 times, function returned a false
    positive, now is fixed

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: jberdugo <jberdugo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 14:10:55 by jberdugo          #+#    #+#             */
-/*   Updated: 2024/06/27 19:23:42 by jberdugo         ###   ########.fr       */
+/*   Updated: 2024/06/28 11:57:57 by jberdugo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,19 @@ typedef struct s_texture_path
 }	t_texture_path;
 
 /******************************************************************************/
-/**
- * mask cheatsheet:
- *   00000000 00000001 -> 0x1        -> NORTH
- *   00000000 00000010 -> 0x2        -> SOUTH
- *   00000000 00000100 -> 0x4        -> WEST
- *   00000000 00001000 -> 0x8        -> EAST
- *   00000000 00010000 -> 0x10       -> FLOOR
- *   00000000 00100000 -> 0x20       -> CEILING
- *  -------------------
- *   00000000 00111111 -> 0x3F       -> ALL_TEXTURES
+/*
+   mask cheatsheet:
+     00000000 00000001 -> 0x1        -> NORTH
+     00000000 00000010 -> 0x2        -> SOUTH
+     00000000 00000100 -> 0x4        -> WEST
+     00000000 00001000 -> 0x8        -> EAST
+     00000000 00010000 -> 0x10       -> FLOOR
+     00000000 00100000 -> 0x20       -> CEILING
+     00000000 00111111 -> 0x3F       -> ALL_TEXTURES
 
- *   100...........000 -> 0x80000000 -> FAILURE
- */
+     100...........000 -> 0x80000000 -> FAILURE
+     010...........000 -> 0x40000000 -> REPEATED
+*/
 typedef enum e_check_flags
 {
 	F_NORTH = 0x1,
@@ -50,6 +50,7 @@ typedef enum e_check_flags
 	F_CEILING = 0x20,
 	ALL_TEXTURES = 0x3F,
 	FAILURE = 0x80000000,
+	REPEATED = 0x40000000
 	//SUCCESS = 
 }	t_check_flags;
 
--- src/parser/check_file_content.c
+++ src/parser/check_file_content.c
@@ -6,7 +6,7 @@
 /*   By: jberdugo <jberdugo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 15:42:25 by jberdugo          #+#    #+#             */
-/*   Updated: 2024/06/28 10:07:14 by jberdugo         ###   ########.fr       */
+/*   Updated: 2024/06/28 11:53:05 by jberdugo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ int	check_file_content(t_list *file)
 /* check that exits one instruction per required texture */
 static t_check_flags	check_all_textures(t_list *file)
 {
-	//FIXME: si ponen 3 del mismo tipo el bitwise XORG daria un falso positivo
 	char			*tmp;
 	t_check_flags	mask;
 
@@ -42,18 +41,22 @@ static t_check_flags	check_all_textures(t_list *file)
 	while (file)
 	{
 		tmp = (char *)(file->content);
-		if (ft_strncmp(tmp, "NO ", 3))
-			mask ^= F_NORTH;
-		else if (ft_strncmp(tmp, "SO ", 3))
-			mask ^= F_SOUTH;
-		else if (ft_strncmp(tmp, "WE ", 3))
-			mask ^= F_WEST;
-		else if (ft_strncmp(tmp, "EA ", 3))
-			mask ^= F_EAST;
-		else if (ft_strncmp(tmp, "F ", 2))
-			mask ^= F_FLOOR;
-		else if (ft_strncmp(tmp, "C ", 2))
-			mask ^= F_CEILING;
+		if (ft_strncmp(tmp, "NO ", 3) && (mask & F_NORTH) != F_NORTH)
+			mask |= F_NORTH;
+		else if (ft_strncmp(tmp, "SO ", 3) && (mask & F_SOUTH) != F_SOUTH)
+			mask |= F_SOUTH;
+		else if (ft_strncmp(tmp, "WE ", 3) && (mask & F_WEST) != F_WEST)
+			mask |= F_WEST;
+		else if (ft_strncmp(tmp, "EA ", 3) && (mask & F_EAST) != F_EAST)
+			mask |= F_EAST;
+		else if (ft_strncmp(tmp, "F ", 2) && (mask & F_FLOOR) != F_FLOOR)
+			mask |= F_FLOOR;
+		else if (ft_strncmp(tmp, "C ", 2) && (mask & F_CEILING) != F_CEILING)
+			mask |= F_CEILING;
+		else if (ft_strncmp(tmp, "NO ", 3) || ft_strncmp(tmp, "SO ", 3)
+			|| ft_strncmp(tmp, "WE ", 3) || ft_strncmp(tmp, "EA ", 3)
+			|| ft_strncmp(tmp, "F ", 3) || ft_strncmp(tmp, "C ", 3))
+			mask |= FAILURE | REPEATED;
 		file = file->next;
 	}
 	if ((mask & ALL_TEXTURES) != ALL_TEXTURES)
@@ -88,4 +91,3 @@ static t_check_flags	check_correct_order(t_list *file)
 	}
 	return (mask);
 }
-
