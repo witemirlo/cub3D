124b7db5e9612b404de435128e87a4005fb16c7b
witemirlo <jacoboberdugo@gmail.com>
2024-07-09 16:02:18 +0200

    [ADD] check if the file passed as argument is a directory

--- src/parser/read_file.c
+++ src/parser/read_file.c
@@ -6,24 +6,34 @@
 /*   By: jberdugo <jberdugo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 12:07:22 by jberdugo          #+#    #+#             */
-/*   Updated: 2024/06/30 12:37:54 by jberdugo         ###   ########.fr       */
+/*   Updated: 2024/07/09 15:53:11 by jberdugo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "colors.h"
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdio.h>
 
 static t_list	*create_list(int fd);
 
 /* Read all source file and returns a t_list * with all its content */
-t_list	*read_file(char const *name)
+t_list	*read_file(char *name)
 {
 	t_list	*list;
 	int		fd;
 
+	fd = open(name, O_RDONLY | O_DIRECTORY);
+	if (fd > 0)
+	{
+		close(fd);
+		ft_putstr_fd(RED"Error: "NC"'", 2);
+		ft_putstr_fd(name, 2);
+		ft_putendl_fd("': is a directory", 2);
+	}
 	fd = open(name, O_RDONLY);
 	if (fd < 0)
 		return (perror("Error"), NULL);
