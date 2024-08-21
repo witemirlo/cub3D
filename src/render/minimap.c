404d7785357a8ebdecaf5c60f45700a8bd60aefa
pablo-is-a-goblin <pablo.sacristan.pernia@gmail.com>
2024-08-21 20:11:20 +0200

    MINIMAP: now it rotates

--- src/render/minimap.c
+++ src/render/minimap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minimap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jberdugo <jberdugo@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: psacrist <psacrist@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 14:41:20 by jberdugo          #+#    #+#             */
-/*   Updated: 2024/08/13 14:47:29 by jberdugo         ###   ########.fr       */
+/*   Updated: 2024/08/21 20:09:35 by psacrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 #include "render.h"
 #include "libft.h"
 
+#include <math.h>
+
 static int	minimap_color(t_vector map_coord, char **map);
 static void	draw_player(int cam_w, int cam_h, mlx_image_t *img);
+t_vector	rot_map(t_vector map, t_vector dir, t_vector pos);
 
 void	minimap(t_data *data)
 {
@@ -30,19 +33,32 @@ void	minimap(t_data *data)
 	i = 0;
 	while (i < cam_w) //mirar rendimiento
 	{
-		map.x = data->player.position.x + \
-			(i + 1 - (double)cam_w / 2) / MINI_TILE;
 		j = 0;
 		while (j < cam_h)
 		{
+			map.x = data->player.position.x + \
+				(i + 1 - (double)cam_w / 2) / MINI_TILE;
 			map.y = data->player.position.y + \
 				(j + 1 - (double)cam_h / 2) / MINI_TILE;
-			mlx_put_pixel(data->minimap, i, j, minimap_color(map, data->map));
+			map = rot_map(map, data->player.direction, data->player.position);
+			if (pow(i - cam_w / 2, 2) + pow(j - cam_h / 2, 2) <= pow(cam_h / 2, 2))
+				mlx_put_pixel(data->minimap, i, j, minimap_color(map, data->map));
 			j++;
 		}
 		i++;
 	}
 	draw_player(cam_w, cam_h, data->minimap);
+}
+
+t_vector	rot_map(t_vector map, t_vector dir, t_vector pos)
+{
+	t_vector	centered;
+	t_vector	transformed;
+	(void)pos;
+	centered = (t_vector){map.x - pos.x, map.y - pos.y};
+	transformed.x = (- dir.x * centered.y - dir.y * centered.x) + pos.x;
+	transformed.y = (dir.x * centered.x - dir.y * centered.y) + pos.y;
+	return (transformed);
 }
 
 static int	minimap_color(t_vector map_coord, char **map)
