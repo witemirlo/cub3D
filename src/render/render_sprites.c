64882a8a0c066797e490cf7bb44ef2412d71202a
pablo-is-a-goblin <pablo.sacristan.pernia@gmail.com>
2024-08-27 17:14:49 +0200

    NEW FEATURE: it can handle more than one sprite on the map

--- src/render/render_sprites.c
+++ src/render/render_sprites.c
@@ -6,7 +6,7 @@
 /*   By: psacrist <psacrist@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 17:36:43 by psacrist          #+#    #+#             */
-/*   Updated: 2024/08/27 14:42:20 by psacrist         ###   ########.fr       */
+/*   Updated: 2024/08/27 16:34:11 by psacrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@
 #include "MLX42.h"
 #include "texturer.h"
 
+#include <math.h>
+
 static int	select_color(double sp_x, double sp_y, mlx_texture_t *sprite);
 void		*ft_lstget(t_list *list, unsigned int index);
 t_vector	rotate_dis(t_vector	sprite, t_vector dir);
+t_vector	*sort_sprites(t_vector *sprites, size_t n, t_vector pos);
 
 #include <stdio.h>
 void	render_sprites(t_data *data, t_list *rays)
@@ -26,52 +29,54 @@ void	render_sprites(t_data *data, t_list *rays)
 	t_vector	*sprites; //ordenados
 
 	t_vector	dis;
-	//t_vector	dis_rot;
 	double		wide_x;
 	int			screen_x;
 	int			line_h;
 	int			sprite_width;
 	int			color;
-	(void)rays;
-	sprites = data->sprites;
-	if (!sprites)
-	{
+	size_t		index;
+
+	if (!data->sprites)
 		return ;
-	}
-	dis = (t_vector){sprites->x - data->player.position.x, - sprites->y + data->player.position.y};
-	dis = rotate_dis(dis, data->player.direction);
-	if (dis.y < 0)
-		return ;
-	wide_x = dis.y * (double)4 / (double)3; //use camera
-	screen_x = ((WIDTH / wide_x) * dis.x) + WIDTH / 2;
-	sprite_width = (WIDTH / wide_x); //tiene que ser igual ma line_h?
-	line_h = (HEIGHT) / dis.y;
-	//if (line_h > HEIGHT)
-	//	line_h = HEIGHT;
-	//printf("\tsprite size: w %d, h %d\n", sprite_width, line_h);
-	//printf("wide_x %f\n", wide_x);
-	//printf("\tsprite loc %d\n", screen_x);
-	//printf("\tdistance to spr: x %f, y %f\n", dis.x, dis.y);
-	for (int i = 0; i < sprite_width; i++)
+	sprites = sort_sprites(data->sprites, data->sprites_size, data->player.position);
+	index = 0;
+	while (index < data->sprites_size)
 	{
-		int x = screen_x - (sprite_width / 2) + i;
-		if (x < 0 || x >= WIDTH)
-			continue;
-		if ((double)HEIGHT / ((t_ray *)(ft_lstget(rays, x)))->wall_len < dis.y)
-			continue;
-		for (int j = 0; j < line_h; j++)
+		dis = (t_vector){sprites[index].x - data->player.position.x, - sprites[index].y + data->player.position.y};
+		dis = rotate_dis(dis, data->player.direction);
+		if (dis.y < 0)
 		{
-			int y = (HEIGHT / 2) - (line_h / 2) + j;
-			if (y < 0 || y >= HEIGHT)
-				continue;
-			color = select_color((double)i / sprite_width, (double)j / line_h, data->textures->sprite);
-			if (color & 0xFF)
-				mlx_put_pixel(data->scene, x, y, color);
+			index++;	
+			continue ;
 		}
+		wide_x = dis.y * (double)4 / (double)3; //use camera
+		screen_x = ((WIDTH / wide_x) * dis.x) + WIDTH / 2;
+		sprite_width = (WIDTH / wide_x); //tiene que ser igual ma line_h?
+		line_h = (HEIGHT) / dis.y;
+		//printf("\tsprite size: w %d, h %d\n", sprite_width, line_h);
+		//printf("wide_x %f\n", wide_x);
+		//printf("\tsprite loc %d\n", screen_x);
+		//printf("\tdistance to spr: x %f, y %f\n", dis.x, dis.y);
+		for (int i = 0; i < sprite_width; i++)
+		{
+			int x = screen_x - (sprite_width / 2) + i;
+			if (x < 0 || x >= WIDTH)
+				continue;
+			if ((double)HEIGHT / ((t_ray *)(ft_lstget(rays, x)))->wall_len < dis.y)
+				continue;
+			for (int j = 0; j < line_h; j++)
+			{
+				int y = (HEIGHT / 2) - (line_h / 2) + j;
+				if (y < 0 || y >= HEIGHT)
+					continue;
+				color = select_color((double)i / sprite_width, (double)j / line_h, data->textures->sprite);
+				if (color & 0xFF)
+					mlx_put_pixel(data->scene, x, y, color);
+			}
+		}
+		index++;
 	}
-	//TODO: multiple
 	//TODO: fisheye
-	//TODO: revisar calculo 
 }
 
 t_vector	rotate_dis(t_vector	sprite, t_vector dir)
@@ -81,6 +86,32 @@ t_vector	rotate_dis(t_vector	sprite, t_vector dir)
 	transformed.x = (- dir.x * sprite.y - dir.y * sprite.x);
 	transformed.y = (dir.x * sprite.x - dir.y * sprite.y);
 	return (transformed);
+}
+
+t_vector	*sort_sprites(t_vector *sprites, size_t n, t_vector pos) //posible leaks
+{
+	size_t		i;
+	size_t		j;
+	t_vector	tmp;
+
+	i = 0;
+	while (i < n)
+	{
+		j = i + 1;
+		while (j < n)
+		{
+			if (pow(sprites[i].x - pos.x, 2) + pow(sprites[i].y -pos.y, 2) < \
+				pow(sprites[j].x - pos.x, 2) + pow(sprites[j].y -pos.y, 2))
+			{
+				tmp = sprites[j];
+				sprites[j] = sprites[i];
+				sprites[i] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (sprites);
 }
 
 void	*ft_lstget(t_list *list, unsigned int index)
