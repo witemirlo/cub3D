cabe83f1a8a404a1c1b1f83c01c5d53dbf4d7afb
pablo-is-a-goblin <pablo.sacristan.pernia@gmail.com>
2024-08-27 19:14:21 +0200

    NORM

--- src/hooks/keyhook.c
+++ src/hooks/keyhook.c
@@ -6,26 +6,26 @@
 /*   By: psacrist <psacrist@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 08:56:16 by psacrist          #+#    #+#             */
-/*   Updated: 2024/08/27 15:48:47 by psacrist         ###   ########.fr       */
+/*   Updated: 2024/08/27 19:14:12 by psacrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hooks.h"
 #include "MLX42.h"
 #include "cub3d.h"
+
 #include <math.h>
 
-void	more_keys(mlx_key_data_t keydata, t_data *param);
-void	get_trig(double *my_sin, double *my_cos);
-void	new_camera(t_player *player);
-void	door_key(t_data *data);
+static void	move_keys(mlx_key_data_t keydata, char **map, t_player *player);
+static void	get_trig(double *my_sin, double *my_cos);
+static void	door_key(t_data *data);
+static void	rotate(int key, t_player *player, double cos_r, double sin_r);
 
 void	keyhook(mlx_key_data_t keydata, void *param)
 {
 	static double	sin_r;
 	static double	cos_r;
 	t_player		*player;
-	t_vector		old_dir;
 
 	if (keydata.key == MLX_KEY_ESCAPE)
 		closehook(param);
@@ -35,30 +35,39 @@ void	keyhook(mlx_key_data_t keydata, void *param)
 		get_trig(&sin_r, &cos_r);
 	if (keydata.key == MLX_KEY_C)
 	{
-		((t_data*)param)->mouse_movement ^= 1;
+		((t_data *)param)->mouse_movement ^= 1;
 		return ;
 	}
 	player = &((t_data *)param)->player;
-	old_dir = player->direction;
-	if (keydata.key == MLX_KEY_RIGHT)
-	{
-		player->direction.x = cos_r * old_dir.x - sin_r * old_dir.y;
-		player->direction.y = sin_r * old_dir.x + cos_r * old_dir.y;
-		new_camera(player);
-	}
-	else if (keydata.key == MLX_KEY_LEFT)
-	{
-		player->direction.x = cos_r * old_dir.x + sin_r * old_dir.y;
-		player->direction.y = -sin_r * old_dir.x + cos_r * old_dir.y;
-		new_camera(player);
-	}
+	if (keydata.key == MLX_KEY_LEFT || keydata.key == MLX_KEY_RIGHT)
+		rotate(keydata.key, player, cos_r, sin_r);
 	if (keydata.key == MLX_KEY_SPACE && keydata.action == MLX_PRESS)
 		door_key(param);
 	else
-		more_keys(keydata, (t_data *)param);
+		move_keys(keydata, ((t_data *)(param))->map, player);
 }
 
-void	door_key(t_data *data)
+static void	rotate(int key, t_player *player, double cos_r, double sin_r)
+{
+	t_vector		old_dir;
+
+	old_dir = player->direction;
+	if (key == MLX_KEY_RIGHT)
+	{
+		player->direction.x = cos_r * old_dir.x - sin_r * old_dir.y;
+		player->direction.y = sin_r * old_dir.x + cos_r * old_dir.y;
+	}
+	else
+	{
+		player->direction.x = cos_r * old_dir.x + sin_r * old_dir.y;
+		player->direction.y = -sin_r * old_dir.x + cos_r * old_dir.y;
+	}
+	player->camera.x = player->direction.y * 2 / 3;
+	player->camera.y = player->direction.x * 2 / 3;
+	player->camera.x *= -1;
+}
+
+static void	door_key(t_data *data)
 {
 	int	target_x;
 	int	target_y;
@@ -71,44 +80,28 @@ void	door_key(t_data *data)
 		data->map[target_y][target_x] = 'C';
 }
 
-void	more_keys(mlx_key_data_t keydata, t_data *data)
+static void	move_keys(mlx_key_data_t keydata, char **map, t_player *player)
 {
 	t_vector	move;
 
-	move = data->player.position;
 	if (keydata.key == MLX_KEY_W)
-	{
-		move.x += data->player.direction.x * MV_FACTOR;
-		move.y += data->player.direction.y * MV_FACTOR;
-	}
+		move = (t_vector){player->direction.x, player->direction.y};
 	else if (keydata.key == MLX_KEY_S)
-	{
-		move.x -= data->player.direction.x * MV_FACTOR;
-		move.y -= data->player.direction.y * MV_FACTOR;
-	}
+		move = (t_vector){-player->direction.x, -player->direction.y};
 	else if (keydata.key == MLX_KEY_A)
-	{
-		move.x -= data->player.camera.x * MV_FACTOR;
-		move.y -= data->player.camera.y * MV_FACTOR;
-	}
+		move = (t_vector){player->direction.y, -player->direction.x};
 	else if (keydata.key == MLX_KEY_D)
-	{
-		move.x += data->player.camera.x * MV_FACTOR;
-		move.y += data->player.camera.y * MV_FACTOR;
-	}
-	if (data->map[(int)move.y][(int)move.x] == '0'
-		|| data->map[(int)move.y][(int)move.x] == 'O')
-		data->player.position = move;
+		move = (t_vector){-player->direction.y, player->direction.x};
+	else
+		return ;
+	move.x = move.x * MV_FACTOR + player->position.x;
+	move.y = move.y * MV_FACTOR + player->position.y;
+	if (map[(int)move.y][(int)move.x] == '0'
+		|| map[(int)move.y][(int)move.x] == 'O')
+		player->position = move;
 }
 
-void	new_camera(t_player *player)
-{
-	player->camera.x = player->direction.y * 2 / 3;
-	player->camera.y = player->direction.x * 2 / 3;
-	player->camera.x *= -1;
-}
-
-void	get_trig(double *my_sin, double *my_cos)
+static void	get_trig(double *my_sin, double *my_cos)
 {
 	*my_sin = sin(MY_PI / ROT_FACTOR);
 	*my_cos = cos(MY_PI / ROT_FACTOR);
